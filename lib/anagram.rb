7ec51325dcbc6adbf4960d507ce4ae3a9a88a162
Michael <mkenton2020@icloud.com>
2021-09-07 01:44:27 -0500

    commit tree 6b9fdabb139d875bad0b2de0948e66169da51d46

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_anagrams_arr)
+        possible_anagrams_arr.select do |possible_anagram|  # select words from array of possible anagrams where following condition is true
+            possible_anagram.split("").sort == @word.split("").sort     # condition for .select is when the possible_anagram and the word from the 
+                                                                        # initialization have the same letters. Use split to get chars and then sort  
+                                                                        # to see if they are the same group of letters
+        end
+    end
+end
